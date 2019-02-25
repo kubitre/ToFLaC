64505c25580e7290dddf00441a83cb824a34f668
kubitre <kubitre@gmail.com>
2019-02-25 12:29:00 +0700

    [UPDATE] mainpage container

--- src/Containers/MainPage/index.jsx
+++ src/Containers/MainPage/index.jsx
@@ -2,9 +2,10 @@ import React, {Component} from 'react';
 import Navbar from '../../Components/Navbar';
 import InputBlockComponent from '../../Components/InputBlock';
 import OutputBlockComponent from '../../Components/OutputBlock';
+import { connect } from 'react-redux';
 
 
-export default class MainPageComponent extends Component{
+class MainPageComponent extends Component{
     render = () => {
         const {enabledOutputBlock} = false
         return (
@@ -15,10 +16,18 @@ export default class MainPageComponent extends Component{
                     <Navbar height="20vh"/>
                 }
                 <div className="blocks_row" style={{display: 'flex', flexDirection: "column", marginLeft: '10px'}}>
-                    <InputBlockComponent heigt="70vh" width="97vw"/>
+                    <InputBlockComponent height="60vh" width="97vw"/>
                     <OutputBlockComponent height="40vh" width="90vw" isEnabled={enabledOutputBlock}/>
                 </div>
             </div>
         )
     }
 }
+
+function mapStateToProps (state) {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps)(MainPageComponent)
